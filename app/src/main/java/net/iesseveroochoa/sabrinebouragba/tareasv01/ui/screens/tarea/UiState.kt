6a40f252d76587ef91632812856ec0b4f2950a44
SabrineBA <sabrinaanhari21@gmail.com>
2024-10-27 19:08:30 +0100

    Inicio refactorizar estadps

--- app/src/main/java/net/iesseveroochoa/sabrinebouragba/tareasv01/ui/screens/tarea/UiState.kt
+++ app/src/main/java/net/iesseveroochoa/sabrinebouragba/tareasv01/ui/screens/tarea/UiState.kt
@@ -1,2 +1,15 @@
 package net.iesseveroochoa.sabrinebouragba.tareasv01.ui.screens.tarea
 
+import androidx.compose.ui.graphics.Color
+
+
+data class UiStateTarea(
+    val categoria :String = "",
+    val prioridad :String = "",
+    val pagado :Boolean = false,
+    val estado :String = "",
+    val valoracion :Int = 0,
+    val tecnico :String = "",
+    val descripcion :String = "",
+    val colorFondo : Color = Color.Transparent
+)
--- app/src/main/java/net/iesseveroochoa/sabrinebouragba/tareasv01/ui/screens/tarea/ViewModel.kt
+++ app/src/main/java/net/iesseveroochoa/sabrinebouragba/tareasv01/ui/screens/tarea/ViewModel.kt
@@ -1,2 +1,43 @@
 package net.iesseveroochoa.sabrinebouragba.tareasv01.ui.screens.tarea
 
+import android.app.Application
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import net.iesseveroochoa.sabrinebouragba.tareasv01.R
+import net.iesseveroochoa.sabrinebouragba.tareasv01.ui.theme.ColorPrioridadAlta
+
+class TareaViewModel(application: Application): AndroidViewModel(application){
+    private val context = application.applicationContext
+
+    // prioridad
+    val listaPrioridad = context.resources.getStringArray(R.array.prioridades_array).toList()
+    val PRIORIDAD_ALTA = listaPrioridad[2]
+
+    private val _uiStateTarea = MutableStateFlow(
+        UiStateTarea(
+            prioridad = listaPrioridad[0],
+        )
+    )
+    val uiStateTarea: StateFlow<UiStateTarea> = _uiStateTarea.asStateFlow()
+
+    fun onValueChangePrioridad(nuevaPrioridad: String){
+        val colorFondo :Color
+        if (PRIORIDAD_ALTA == nuevaPrioridad){
+            colorFondo = ColorPrioridadAlta
+        } else {
+            colorFondo = Color.Transparent
+        }
+
+        _uiStateTarea.value = _uiStateTarea.value.copy(
+            prioridad = nuevaPrioridad,
+            colorFondo = colorFondo
+        )
+
+    }
+
+}
+
+
