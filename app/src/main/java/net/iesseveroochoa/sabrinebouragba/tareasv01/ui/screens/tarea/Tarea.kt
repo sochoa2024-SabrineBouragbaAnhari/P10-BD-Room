70dc1626eb603a0dc28121d0d4bf60ab28a4c506
SabrineBA <sabrinaanhari21@gmail.com>
2024-10-27 18:24:19 +0100

    Inicio práctica

--- app/src/main/java/net/iesseveroochoa/sabrinebouragba/tareasv01/ui/screens/tarea/Tarea.kt
+++ app/src/main/java/net/iesseveroochoa/sabrinebouragba/tareasv01/ui/screens/tarea/Tarea.kt
@@ -1,7 +1,6 @@
 package net.iesseveroochoa.sabrinebouragba.tareasv01.ui.screens.tarea
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
